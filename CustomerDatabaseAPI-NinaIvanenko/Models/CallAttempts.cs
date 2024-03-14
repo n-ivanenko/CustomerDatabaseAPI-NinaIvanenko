c2783c613771957b15a6ae4f970366ef0c746e52
ninaivanenko <n.ivanenko@students.clark.edu>
2024-03-13 17:17:30 -0700

    Initial commmit

---
+++ CustomerDatabaseAPI-NinaIvanenko/Models/CallAttempts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace customer_database_API_Nina.Models
+{
+    public class CallAttempt
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime AttemptDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ CustomerDatabaseAPI-NinaIvanenko/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace customer_database_API_Nina.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
